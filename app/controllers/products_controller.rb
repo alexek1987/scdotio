f4c7bc9e6377e0c00c38c289bece6e44236cb61a
Alexander <alexander@Alex.local>
2020-08-22 01:03:48 +0700

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,6 @@ class ProductsController < ApplicationController
         lat: product.latitude,
         lng: product.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { product: product }),
-        image_url: helpers.asset_url('dogpin'),
         id: product.id
       }
    end
